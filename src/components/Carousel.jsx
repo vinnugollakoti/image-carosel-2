07bb0b84a9575af2070c26327a26c1d926cbe964
vinay reddy gollakoti <145451307+vinnugollakoti@users.noreply.github.com>
2024-02-01 15:25:21 +0530

    Add files via upload

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Carousel.css";
+import { images } from "../data/CarouselData";
+// you can explore more - and check as how to use materiul ui
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+function Carousel() {
+    const image=images
+    let [curr,setCurr]=useState(0)
+    return(
+        <>
+            <img src={image[curr].img}></img>
+            <h1 className="title">{image[curr].title}</h1>
+            <h2 className="des">{image[curr].subtitle}</h2>
+            <div className="bu">
+            <ArrowBackIosIcon onClick={()=>{
+                let check = curr == 0 ? 2 : curr-1;
+                setCurr(check)
+            }} className="no"/>
+
+            <ArrowForwardIosIcon onClick={()=>{
+                let check=curr == 2 ? 0 : curr+1;
+                  setCurr(check);
+            }} className="no"/>
+             </div>
+        </>
+    )
+
+    
+}
+
+export default Carousel;
